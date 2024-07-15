a5a97eaca1d4b25f74280c82284d03d6ca598823
Yuta Sukekawa <sukekawa@thymefield.jp>
2024-07-15 16:27:11 +0900

    feat: add loading lazy props

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <h1>Testing <a href="https://image.nuxt.com/">NuxtImage</a> on Vercel.</h1>
-    <NuxtImg src="/logo.svg" format="webp" />
+    <template v-for="i in 10" :key="`${i}-i`">
+      <NuxtImg src="/logo.svg" format="webp" loading="lazy" /><br />
+    </template>
   </div>
 </template>
